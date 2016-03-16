0acf3efea892573592402ed52e94593b289eaff3
Scott Cooper <scttcper@gmail.com>
2016-03-16 00:19:15 -0700

    copy from angular/material2

---
+++ src/demo-app/demo-app.ts
@@ -0,0 +1,23 @@
+import {Component} from 'angular2/core';
+import {Route, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Dir} from '../core/rtl/dir';
+
+
+@Component({
+  selector: 'home',
+  templateUrl: 'demo-app/home.html'
+})
+export class Home {}
+
+@Component({
+  selector: 'demo-app',
+  providers: [],
+  templateUrl: 'demo-app/demo-app.html',
+  styleUrls: ['demo-app/demo-app.css'],
+  directives: [ROUTER_DIRECTIVES, Dir],
+  pipes: []
+})
+@RouteConfig([
+  new Route({path: '/', name: 'Home', component: Home, useAsDefault: true})
+])
+export class DemoApp { }
