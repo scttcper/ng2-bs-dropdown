e45356110ad12627f518a3eb016e2753e073910e
Scott Cooper <scttcper@gmail.com>
2016-03-17 23:43:48 -0700

    use bootstrap

--- src/demo-app/demo-app.ts
+++ src/demo-app/demo-app.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, ViewEncapsulation} from 'angular2/core';
 import {Route, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Dir} from '../core/rtl/dir';
 
@@ -15,6 +15,7 @@ export class Home {}
   templateUrl: 'demo-app/demo-app.html',
   styleUrls: ['demo-app/demo-app.css'],
   directives: [ROUTER_DIRECTIVES, Dir],
+  encapsulation: ViewEncapsulation.None,
   pipes: []
 })
 @RouteConfig([
