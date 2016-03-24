08651fea5723ca4e7c5971b51f4f0a6bc5be8409
Scott Cooper <scttcper@gmail.com>
2016-03-24 15:27:36 -0700

    Updates

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 var browser_1 = require('angular2/platform/browser');
-var core_1 = require('angular2/core');
 var demo_app_1 = require('./demo-app/demo-app');
 var router_1 = require('angular2/router');
 browser_1.bootstrap(demo_app_1.DemoApp, [
-    router_1.ROUTER_PROVIDERS,
-    core_1.provide(router_1.APP_BASE_HREF, { useValue: '/' })
+    router_1.ROUTER_PROVIDERS
 ]);
 //# sourceMappingURL=../../main.js.map
--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 import {bootstrap} from 'angular2/platform/browser';
-import {provide} from 'angular2/core';
 import {DemoApp} from './demo-app/demo-app';
-import {ROUTER_PROVIDERS, APP_BASE_HREF} from 'angular2/router';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {APP_BASE_HREF} from 'angular2/router';
 
 bootstrap(DemoApp, [
-  ROUTER_PROVIDERS,
-  provide(APP_BASE_HREF, { useValue: '/' })
+  ROUTER_PROVIDERS
 ]);
