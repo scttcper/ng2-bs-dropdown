7eed8404cca20a75ba840aac04ef222341fb405c
Scott Cooper <scttcper@gmail.com>
2016-07-06 10:45:17 -0700

    clean up more from material2

--- stage-release.sh
+++ stage-release.sh
@@ -18,18 +18,11 @@ npm run ng build
 # deploy/ serves as a working directory to stage the release.
 mkdir deploy
 
-# Copy all components/ to deploy/ and replace `../../core` with `@angular2-material/core`
-# that each component depends on the core *package*.
-# Use a `.bak` extension for sed backup because `sed` on OSX will not work without a backup
-# extension. Delete the backups immediately after.
+# Copy all components/ to deploy/
 cp -R ./dist/components/* ./deploy/
-find ./deploy -type f \( -name "*.js" -o -name "*.ts" \) -exec sed -i.bak 's|\.\./\.\./core|@angular2-material/core|g' {} \;
+
+# copy readme
 for d in ./deploy/*; do cp README.md "$d"; done
 
-
-find ./deploy -type f -name "*.bak" | xargs rm
-
-# Copy the core/ directory directly into ./deploy
-# cp -R ./dist/core/ ./deploy/core/
 
 # To test the packages, simply `npm install` the package directories.
